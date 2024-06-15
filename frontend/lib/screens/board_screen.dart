e2229af8d761bd2c6f92ee9acf060bcef32234f7
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-06-16 03:03:05 +0900

    feat : #17 숨김 목록을 화면에 구현
    - 숨김해제 시 selectedHiddenItems에 추가해  removewhere 메서드로 숨김 목록 화면에 제거 구현
    - hiddenList에 제거

--- frontend/lib/screens/board_screen.dart
+++ frontend/lib/screens/board_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:frontend/screens/hiddenList_screen.dart';
 import 'package:frontend/screens/home_screen.dart';
 import 'package:frontend/screens/write_screen.dart';
-import 'package:frontend/widgets/gradeBoard_widget.dart';
+import 'package:frontend/widgets/board_widget.dart';
 import 'package:frontend/widgets/levelBtn_widget.dart';
 
 class BoardPage extends StatefulWidget {
@@ -25,18 +25,21 @@ class _BoardPageState extends State<BoardPage> {
       'subtitle': '정보통신공학과 증원 신청',
       'content': '1차 : 5일 오후 2시까지 신청',
       'isChecked': false,
+      'originalIndex': 0,
     },
     {
       'title': '새내기 게시판',
       'subtitle': '수강 신청 하는 방법 알려주세요',
       'content': '수강 신청 잘 할 수 있을까요?',
       'isChecked': false,
+      'originalIndex': 1,
     },
     {
       'title': '제자반 채플 자주 묻는 질문',
       'subtitle': '채플 담당 교수님 연락처는 어떻게 알아요?',
       'content': '스카이 시스템 -> 학적 정보 -> 학적 정보 조회 -> 지도 교수(전화번호...',
       'isChecked': false,
+      'originalIndex': 2,
     },
   ];
 
@@ -46,18 +49,21 @@ class _BoardPageState extends State<BoardPage> {
       'subtitle': '정보통신공학과 증원 신청',
       'content': '2차 : 5일 오후 2시까지 신청',
       'isChecked': false,
+      'originalIndex': 0,
     },
     {
       'title': '헌내기 게시판',
       'subtitle': '수강 신청 하는 방법 알려주세요',
       'content': '수강 신청 잘 할 수 있을까요?',
       'isChecked': false,
+      'originalIndex': 1,
     },
     {
       'title': '사회봉사 자주 묻는 질문',
       'subtitle': '사회봉사 교수님 연락처는 어떻게 알아요?',
       'content': '스카이 시스템 -> 학적 정보 -> 학적 정보 조회 -> 지도 교수(전화번호...',
       'isChecked': false,
+      'originalIndex': 2,
     },
   ];
 
@@ -67,18 +73,21 @@ class _BoardPageState extends State<BoardPage> {
       'subtitle': '정보통신공학과 증원 신청',
       'content': '1차 : 5일 오후 2시까지 신청',
       'isChecked': false,
+      'originalIndex': 0,
     },
     {
       'title': '암모니아 게시판',
       'subtitle': '복학 신청 하는 방법 알려주세요',
       'content': '복학 신청 잘 할 수 있을까요?',
       'isChecked': false,
+      'originalIndex': 1,
     },
     {
       'title': '현대인과 기독교 자주 묻는 질문',
       'subtitle': '현기 교수님 연락처는 어떻게 알아요?',
       'content': '스카이 시스템 -> 학적 정보 -> 학적 정보 조회 -> 지도 교수(전화번호...',
       'isChecked': false,
+      'originalIndex': 2,
     },
   ];
 
@@ -88,22 +97,46 @@ class _BoardPageState extends State<BoardPage> {
       'subtitle': '정보통신공학과 증원 신청',
       'content': '1차 : 5일 오후 2시까지 신청',
       'isChecked': false,
+      'originalIndex': 0,
     },
     {
       'title': '졸업반 게시판',
       'subtitle': '자기소개서 작성 하는 방법 알려주세요',
       'content': '자소서 작성 잘 할 수 있을까요?',
       'isChecked': false,
+      'originalIndex': 1,
     },
     {
       'title': '졸업작품 자주 묻는 질문',
       'subtitle': '졸업작품 담당 교수님 연락처는 어떻게 알아요?',
       'content': '스카이 시스템 -> 학적 정보 -> 학적 정보 조회 -> 지도 교수(전화번호...',
       'isChecked': false,
+      'originalIndex': 2,
     },
   ];
 
   List<Map<String, dynamic>> hiddenList = [];
+
+  // 숨김 해제 시 호출되는 함수
+  void unhideItems(List<Map<String, dynamic>> items) {
+    setState(() {
+      for (var item in items) {
+        item['isChecked'] = false;
+        // 각 학년별 게시판 리스트 (oneBoard, twoBoard, threeBoard, fourBoard)를 정의
+        // 각 항목에 originalIndex를 추가하여 원래 인덱스를 저장
+        // 각 항목의 originalIndex를 사용하여 원래 인덱스 위치에 항목을 다시 추가
+        if (selectedGrade == '1학년') {
+          oneBoard.insert(item['originalIndex'], item);
+        } else if (selectedGrade == '2학년') {
+          twoBoard.insert(item['originalIndex'], item);
+        } else if (selectedGrade == '3학년') {
+          threeBoard.insert(item['originalIndex'], item);
+        } else if (selectedGrade == '4학년') {
+          fourBoard.insert(item['originalIndex'], item);
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -191,10 +224,10 @@ class _BoardPageState extends State<BoardPage> {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => HiddenPage(
-                              hiddenList: hiddenList,
-                            ),
-                          ),
+                              builder: (context) => HiddenPage(
+                                    hiddenList: hiddenList,
+                                    onUnhide: unhideItems,
+                                  )),
                         );
                       }),
                     ];
@@ -261,7 +294,7 @@ class _BoardPageState extends State<BoardPage> {
             // 해당 학년 공지 표시
             if (selectedGrade == '1학년')
               // 전달받은 isEdited 값을 isHidDel 값에 저장
-              GradeBoard(
+              Board(
                 boardList: oneBoard,
                 onChecked: (isPressed) {
                   setState(() {
@@ -271,7 +304,7 @@ class _BoardPageState extends State<BoardPage> {
                 isShowed: isHidDel,
               )
             else if (selectedGrade == '2학년')
-              GradeBoard(
+              Board(
                 boardList: twoBoard,
                 onChecked: (isPressed) {
                   setState(() {
@@ -281,7 +314,7 @@ class _BoardPageState extends State<BoardPage> {
                 isShowed: isHidDel,
               )
             else if (selectedGrade == '3학년')
-              GradeBoard(
+              Board(
                 boardList: threeBoard,
                 onChecked: (isPressed) {
                   setState(() {
@@ -291,7 +324,7 @@ class _BoardPageState extends State<BoardPage> {
                 isShowed: isHidDel,
               )
             else if (selectedGrade == '4학년')
-              GradeBoard(
+              Board(
                 boardList: fourBoard,
                 onChecked: (isPressed) {
                   setState(() {
@@ -339,7 +372,7 @@ class _BoardPageState extends State<BoardPage> {
                         fourBoard
                             .removeWhere((board) => board['isChecked'] == true);
                       }
-                      isHidDel = false; // 숨김/삭제 작업이 완료되면 버튼 비활성화로 숨김
+                      isHidDel = false; // 숨김/삭제 버튼 비활성화
                     });
                   },
                   style: ElevatedButton.styleFrom(
--- frontend/lib/screens/hiddenList_screen.dart
+++ frontend/lib/screens/hiddenList_screen.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 
 class HiddenPage extends StatefulWidget {
   final List<Map<String, dynamic>> hiddenList;
-  const HiddenPage({required this.hiddenList, super.key});
+  final Function(List<Map<String, dynamic>>) onUnhide;
+  const HiddenPage(
+      {required this.hiddenList, required this.onUnhide, super.key});
 
   @override
   State<HiddenPage> createState() => _HiddenPageState();
 }
 
 class _HiddenPageState extends State<HiddenPage> {
+  bool isShowed = false;
+  List<Map<String, dynamic>> selectedHiddenItems = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,66 +81,102 @@ class _HiddenPageState extends State<HiddenPage> {
                 itemBuilder: (context, index) {
                   return Column(
                     children: [
-                      Card(
-                        color: const Color(0xFFFAFAFE),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        elevation: 0.5,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20.0, vertical: 18.0),
-                          width: double.infinity,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    widget.hiddenList[index]['title'],
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
+                      GestureDetector(
+                        onLongPress: () {
+                          setState(() {
+                            isShowed = !isShowed;
+                          });
+                        },
+                        child: Card(
+                          color: const Color(0xFFFAFAFE),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          elevation: 0.5,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 18.0),
+                            width: double.infinity,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      widget.hiddenList[index]['title'],
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
+                                    isShowed
+                                        ? SizedBox(
+                                            height: 20,
+                                            width: 20,
+                                            child: Checkbox(
+                                              value:
+                                                  // selectedHiddenItems에 hiddenList[index]이 포함되어 있으면 true
+                                                  // 포함되어 있지 않으면 false 반환
+                                                  selectedHiddenItems.contains(
+                                                      widget.hiddenList[index]),
+                                              onChanged: (value) {
+                                                setState(() {
+                                                  if (value == true) {
+                                                    selectedHiddenItems.add(
+                                                        widget
+                                                            .hiddenList[index]);
+                                                  } else {
+                                                    selectedHiddenItems.remove(
+                                                        widget
+                                                            .hiddenList[index]);
+                                                  }
+                                                });
+                                              },
+                                              shape: const CircleBorder(),
+                                              activeColor:
+                                                  const Color(0xFF7B88C2),
+                                            ),
+                                          )
+                                        : Container(),
+                                  ],
+                                ),
+                                const SizedBox(height: 7),
+                                Text(
+                                  widget.hiddenList[index]['subtitle'],
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
                                   ),
-                                ],
-                              ),
-                              const SizedBox(height: 7),
-                              Text(
-                                widget.hiddenList[index]['subtitle'],
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
                                 ),
-                              ),
-                              const SizedBox(height: 7),
-                              Text(
-                                widget.hiddenList[index]['content'],
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xFF7D7D7F),
+                                const SizedBox(height: 7),
+                                Text(
+                                  widget.hiddenList[index]['content'],
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFF7D7D7F),
+                                  ),
                                 ),
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  IconButton(
-                                    padding: EdgeInsets.zero, // 패딩 설정
-                                    constraints: const BoxConstraints(),
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.favorite_border,
-                                      color: Color(0xFFEA4E44),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                      padding: EdgeInsets.zero, // 패딩 설정
+                                      constraints: const BoxConstraints(),
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        Icons.favorite_border,
+                                        color: Color(0xFFEA4E44),
+                                      ),
                                     ),
-                                  ),
-                                  const Text(
-                                    '4',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ],
-                              ),
-                            ],
+                                    const Text(
+                                      '4',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
@@ -146,6 +187,41 @@ class _HiddenPageState extends State<HiddenPage> {
               ),
             ),
           ],
+        ),
+      ),
+      bottomNavigationBar: ElevatedButton(
+        onPressed: selectedHiddenItems.isNotEmpty
+            ? () {
+                // selectedHiddenItems에 있는 항목들을 hiddenList에 제거해 숨김 목록 취소
+                widget.onUnhide(selectedHiddenItems);
+                setState(() {
+                  widget.hiddenList.removeWhere((item) =>
+                      selectedHiddenItems.contains(
+                          item)); // selectedHiddenItems에 있는 항목들을 필터링해 숨김 목록 화면에 제거
+                  // 그 이후에 selectedHiddenItems에 있는 항목들 제거
+                  selectedHiddenItems.clear();
+                  isShowed = false;
+                });
+              }
+            : null,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: selectedHiddenItems.isNotEmpty
+              ? const Color(0xFF2B72E7)
+              : const Color(0xFFFAFAFE),
+          minimumSize: const Size(205, 75),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(0),
+          ),
+        ),
+        child: Text(
+          '숨김 해제',
+          style: TextStyle(
+            color: selectedHiddenItems.isNotEmpty
+                ? Colors.white
+                : Colors.black.withOpacity(0.5),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
