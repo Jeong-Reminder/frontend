bb9a864224ca75b9e99264dbff8d81cbc0212136
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-06-20 17:34:13 +0900

    feat : #44 카메라와 갤러리 연동
    - imagepicker 패키지 사용
    - Info.plist파일에서 카메라, 사진, 마이크 권한 코드를 추가해야 틩김 현상 오류 해결

--- frontend/lib/screens/profileSetting_screen.dart
+++ frontend/lib/screens/profileSetting_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileSettingPage extends StatefulWidget {
   const ProfileSettingPage({super.key});
@@ -8,6 +12,39 @@ class ProfileSettingPage extends StatefulWidget {
 }
 
 class _ProfileSettingPageState extends State<ProfileSettingPage> {
+  // build 메서드 안에 선언을 하면 상태가 변경될 때마다 변수가 초기화되기 때문에 null 체크가 항상 true로 평가되서 build 메서드 밖에서 정의
+  XFile? pickedImage; // 파일 받을 변수
+
+  // 카메라 연동 함수
+  void getCameraImage() async {
+    final pickedCamera =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+    if (pickedCamera != null) {
+      setState(() {
+        pickedImage = pickedCamera;
+      });
+    } else {
+      if (kDebugMode) {
+        print('이미지 선택안함');
+      }
+    }
+  }
+
+  // 갤러리 연동 함수
+  void getGalleryImage() async {
+    final pickedGallery =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedGallery != null) {
+      setState(() {
+        pickedImage = pickedGallery;
+      });
+    } else {
+      if (kDebugMode) {
+        print('이미지 선택안함');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final imageSize = MediaQuery.of(context).size.width / 4; // 전체 화면 너비의 4분의 1
@@ -36,73 +73,31 @@ class _ProfileSettingPageState extends State<ProfileSettingPage> {
               const SizedBox(height: 45),
 
               // 프로필
-              IconButton(
-                onPressed: () {
-                  showModalBottomSheet(
-                    context: context,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(
-                        top: Radius.circular(25),
+              if (pickedImage == null)
+                IconButton(
+                  onPressed: () {
+                    wayToChooseImage(context);
+                  },
+                  icon: Icon(
+                    Icons.account_circle,
+                    size: imageSize,
+                  ),
+                )
+              else
+                Container(
+                  width: imageSize,
+                  height: imageSize,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                        color: Theme.of(context).colorScheme.primary, width: 2),
+                    image: DecorationImage(
+                      image: FileImage(
+                        File(pickedImage!.path),
                       ),
                     ),
-                    backgroundColor: Colors.white,
-                    builder: (context) {
-                      return Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          // 카메라 버튼
-                          TextButton.icon(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                              minimumSize: const Size(double.infinity, 80),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(0.0)),
-                            ),
-                            icon: const Icon(
-                              Icons.camera_alt,
-                              color: Colors.black,
-                            ),
-                            label: const Text(
-                              '카메라',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                          const Divider(
-                            thickness: 3,
-                          ),
-
-                          // 라이브러리 버튼
-                          TextButton.icon(
-                            onPressed: () {},
-                            style: TextButton.styleFrom(
-                                minimumSize: const Size(double.infinity, 80),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(0.0))),
-                            icon: const Icon(
-                              Icons.photo_album_outlined,
-                              color: Colors.black,
-                            ),
-                            label: const Text(
-                              '라이브러리에서 불러오기',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                icon: Icon(
-                  Icons.account_circle,
-                  size: imageSize,
+                  ),
                 ),
-              ),
               const SizedBox(height: 74),
 
               // 알리미 시작하기 버튼
@@ -130,6 +125,73 @@ class _ProfileSettingPageState extends State<ProfileSettingPage> {
           ),
         ),
       ),
+    );
+  }
+
+  // 이미지 선택 함수
+  Future<dynamic> wayToChooseImage(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(25),
+        ),
+      ),
+      // backgroundColor: Colors.white,
+      builder: (context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // 카메라 버튼
+            TextButton.icon(
+              onPressed: () {
+                getCameraImage();
+              },
+              style: TextButton.styleFrom(
+                minimumSize: const Size(double.infinity, 80),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0.0)),
+              ),
+              icon: const Icon(
+                Icons.camera_alt,
+                color: Colors.black,
+              ),
+              label: const Text(
+                '카메라',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            const Divider(
+              thickness: 3,
+            ),
+
+            // 라이브러리 버튼
+            TextButton.icon(
+              onPressed: () {
+                getGalleryImage();
+              },
+              style: TextButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 80),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0.0))),
+              icon: const Icon(
+                Icons.photo_album_outlined,
+                color: Colors.black,
+              ),
+              label: const Text(
+                '갤러리',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
