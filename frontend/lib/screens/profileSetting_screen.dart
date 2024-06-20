b0d8467709434ef6fc1391dd59d6d6c579ec9745
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-06-20 18:14:43 +0900

    fix : #44 프로필 설정 페이지 UI 재구현
    - 모달시트 대신 카메라와 갤러리 버튼을 만들어 연동하게 구현
    - 알리미 시작하기 버튼을 우측 하단으로 위치 지정

--- frontend/lib/screens/profileSetting_screen.dart
+++ frontend/lib/screens/profileSetting_screen.dart
@@ -52,146 +52,119 @@ class _ProfileSettingPageState extends State<ProfileSettingPage> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(top: 200),
-        child: Center(
-          child: Column(
-            children: [
-              const Text(
-                '프로필 설정',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 14),
-              const Text(
-                '나중에 언제든지 변경할 수 있습니다',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Color(0xFF808080),
-                ),
-              ),
-              const SizedBox(height: 45),
-
-              // 프로필
-              if (pickedImage == null)
-                IconButton(
-                  onPressed: () {
-                    wayToChooseImage(context);
-                  },
-                  icon: Icon(
-                    Icons.account_circle,
-                    size: imageSize,
-                  ),
-                )
-              else
-                Container(
-                  width: imageSize,
-                  height: imageSize,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                        color: Theme.of(context).colorScheme.primary, width: 2),
-                    image: DecorationImage(
-                      image: FileImage(
-                        File(pickedImage!.path),
-                      ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  const Text(
+                    '프로필 설정',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              const SizedBox(height: 74),
-
-              // 알리미 시작하기 버튼
-              ElevatedButton(
-                onPressed: () {
-                  // 이미지 설정해야만 이동
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFDBE7FB),
-                  minimumSize: const Size(211, 40),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0),
+                  const SizedBox(height: 14),
+                  const Text(
+                    '나중에 언제든지 변경할 수 있습니다',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFF808080),
+                    ),
                   ),
-                ),
-                child: const Text(
-                  '알리미 시작하기',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
+                  const SizedBox(height: 45),
+
+                  // 프로필
+                  if (pickedImage == null)
+                    Icon(
+                      Icons.account_circle,
+                      size: imageSize,
+                    )
+                  else
+                    Container(
+                      width: imageSize,
+                      height: imageSize,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                            color: Theme.of(context).colorScheme.primary,
+                            width: 2),
+                        image: DecorationImage(
+                          image: FileImage(
+                            File(pickedImage!.path),
+                          ),
+                        ),
+                      ),
+                    ),
+                  const SizedBox(height: 74),
+
+                  // 카메라와 갤러리 연동 버튼
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      getImageButton(() {
+                        getCameraImage();
+                      }, Icons.camera_alt_rounded),
+                      const SizedBox(width: 50),
+                      getImageButton(() {
+                        getGalleryImage();
+                      }, Icons.photo_album_outlined),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 70),
+
+            // 알리미 시작하기 버튼
+            GestureDetector(
+              onTap: () {
+                // 프로필 설정해야만 이동
+                // 설정을 안하고 이동할 시 프로필을 설정하세요라고 입력
+              },
+              child: const Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    '알리미 시작하기',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(width: 8), // 텍스트와 아이콘 사이의 간격
+                  Icon(
+                    Icons.chevron_right_sharp,
+                    size: 40.0,
                     color: Colors.black,
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  // 이미지 선택 함수
-  Future<dynamic> wayToChooseImage(BuildContext context) {
-    return showModalBottomSheet(
-      context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(25),
+  // 카메라 / 갤러리 연동 버튼 함수
+  Widget getImageButton(VoidCallback getImage, IconData icon) {
+    return ElevatedButton(
+      onPressed: getImage,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xFFDBE7FB),
+        minimumSize: const Size(20, 60),
+        elevation: 3.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
         ),
       ),
-      // backgroundColor: Colors.white,
-      builder: (context) {
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            // 카메라 버튼
-            TextButton.icon(
-              onPressed: () {
-                getCameraImage();
-              },
-              style: TextButton.styleFrom(
-                minimumSize: const Size(double.infinity, 80),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0.0)),
-              ),
-              icon: const Icon(
-                Icons.camera_alt,
-                color: Colors.black,
-              ),
-              label: const Text(
-                '카메라',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            const Divider(
-              thickness: 3,
-            ),
-
-            // 라이브러리 버튼
-            TextButton.icon(
-              onPressed: () {
-                getGalleryImage();
-              },
-              style: TextButton.styleFrom(
-                  minimumSize: const Size(double.infinity, 80),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(0.0))),
-              icon: const Icon(
-                Icons.photo_album_outlined,
-                color: Colors.black,
-              ),
-              label: const Text(
-                '갤러리',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ],
-        );
-      },
+      child: Icon(
+        icon,
+        color: Colors.white,
+      ),
     );
   }
 }
