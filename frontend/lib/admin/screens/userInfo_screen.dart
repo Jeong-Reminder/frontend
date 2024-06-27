63a9f2ef832552cad86df239a4ac0871b10961dc
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-06-27 16:32:48 +0900

    feat(admin) : #60 파일 선택 기능 구현
    - file_picker 패키지 사용
    - 여러개의 엑셀 파일만 선택할 수 있도록 구현

--- frontend/lib/admin/screens/userInfo_screen.dart
+++ frontend/lib/admin/screens/userInfo_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+import 'package:excel/excel.dart';
 
 class UserInfoPage extends StatefulWidget {
   const UserInfoPage({super.key});
@@ -9,6 +12,47 @@ class UserInfoPage extends StatefulWidget {
 
 class _UserInfoPageState extends State<UserInfoPage> {
   TextEditingController searchController = TextEditingController(); // 검색 컨트롤러
+
+  void pickFiles() async {
+    // 1. 파일 선택기 열기(FilePicker.platform.pickFiles)
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom, // 특정 파일만 선택 가능하게 설정
+      allowedExtensions: ['xlsx'], // xlsx 확장 파일만 선택 가능하게 설정
+      allowMultiple: true, // 여러 개의 파일을 선택할 수 있도록 설정
+    );
+
+    // 파일을 선택했을 경우 실행
+    if (result != null) {
+      for (var selectedFile in result.files) {
+        // 2. 선택된 파일 처리
+        // 선택한 파일의 경로를 File 객체 file에 저장
+        File file = File(selectedFile.path!);
+
+        // 선택한 엑셀 파일을 바이트 배열로 변환(readAsBytesSync)
+        var bytes = file.readAsBytesSync();
+
+        // 엑셀의 바이트 데이터를 Excel 객체로 디코드(decodeBytes)
+        var excel = Excel.decodeBytes(bytes);
+
+        // 3. Excel 데이터 처리
+        // 엑셀 파일의 각 시트에 대해 반복문을 돌림
+        for (var table in excel.tables.keys) {
+          print('Sheet: $table'); // 시트 이름(table)
+          print(
+              'Max Columns: ${excel.tables[table]?.maxColumns}'); // 시트 최대 열(excel.tables[table]?.maxColumns)
+          print(
+              'Max Rows: ${excel.tables[table]?.maxRows}'); // 시트 최대 행(excel.tables[table]?.maxRows)
+
+          // 시트의 각 행에 대해 반복문을 돌림
+          for (var row in excel.tables[table]!.rows) {
+            print('$row'); // 각 행에 대해 데이터 출력
+          }
+        }
+      }
+    } else {
+      print('파일 선택 안됨');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -75,13 +119,13 @@ class _UserInfoPageState extends State<UserInfoPage> {
               constraints: const BoxConstraints(
                   minWidth: double.infinity, minHeight: 35), // 검색 바 크기
               elevation: const MaterialStatePropertyAll(0), // 그림
-              shape: MaterialStateProperty.all(
+              shape: MaterialStatePropertyAll(
                 RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(5),
                 ),
               ),
-              padding: MaterialStateProperty.all(
-                const EdgeInsets.symmetric(horizontal: 5),
+              padding: const MaterialStatePropertyAll(
+                EdgeInsets.symmetric(horizontal: 5),
               ),
               trailing: [
                 Image.asset('assets/images/send.png'),
@@ -137,7 +181,7 @@ class _UserInfoPageState extends State<UserInfoPage> {
 
                 // 파일 불러오기 버튼
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: pickFiles,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color(0xFFE6E6E6),
                     minimumSize: const Size(45, 45),
