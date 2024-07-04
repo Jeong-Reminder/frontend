bdc6d1732bcd50fd4e063999e0020dd29a2f8f9e
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-07-04 10:50:17 +0900

    feat : #72 각 팀원 프로젝트 경험 보는 페이지 구현 - ExpansionTile 위젯을 사용해서 리스트를 펼쳐서 더 많은 정보를 볼 수 있게 구현

--- frontend/lib/screens/experience_screen.dart
+++ frontend/lib/screens/experience_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-class ExperiencePage extends StatelessWidget {
+class ExperiencePage extends StatefulWidget {
   const ExperiencePage({super.key});
 
+  @override
+  State<ExperiencePage> createState() => _ExperiencePageState();
+}
+
+class _ExperiencePageState extends State<ExperiencePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         toolbarHeight: 70,
+        backgroundColor: Colors.white,
         leading: BackButton(
           onPressed: () {
             Navigator.pop(context);
@@ -39,19 +45,38 @@ class ExperiencePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
-              '사용자 경험',
+              '✨(이름)의 빛나는 경험✨',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
             ),
             const SizedBox(height: 20),
-            userInfo(
-                '프로젝트명', '음식점 사장님들을 위한 chatGPT를 이용한 AI 기반 운영꿀팁 다이닝 어플리케이션'),
-            userInfo('나의 역할', '팀장'),
-            userInfo('프로젝트 경험', '프로젝트 경험에 대한 것...'),
-            userInfo('깃허브 프로젝트 링크', 'https://github.com/Jeong-Reminder/'),
-            userInfo('프로젝트 기간', '1개월'),
+            ExpansionTile(
+              title: userInfo(
+                  title: '프로젝트명',
+                  titleSize: 20,
+                  info: '음식점 사장님들을 위한 chatGPT를 이용한 AI 기반 운영꿀팁 다이닝 어플리케이션'),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Column(
+                    children: [
+                      userInfo(title: '나의 역할', titleSize: 18, info: '팀장'),
+                      userInfo(
+                          title: '프로젝트 경험',
+                          titleSize: 18,
+                          info: '프로젝트 경험에 대한 것...'),
+                      userInfo(
+                          title: '깃허브 프로젝트 링크',
+                          titleSize: 18,
+                          info: 'https://github.com/Jeong-Reminder/'),
+                      userInfo(title: '프로젝트 기간', titleSize: 18, info: '1개월'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
@@ -59,18 +84,20 @@ class ExperiencePage extends StatelessWidget {
   }
 
   // 회원 정보 위젯
-  Widget userInfo(String title, String info) {
+  Widget userInfo(
+      {required String title,
+      required double titleSize,
+      required String info}) {
     return Column(
       children: [
-        const Divider(),
         ListTile(
           contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
           title: Padding(
             padding: const EdgeInsets.only(bottom: 20.0),
             child: Text(
               title,
-              style: const TextStyle(
-                fontSize: 20,
+              style: TextStyle(
+                fontSize: titleSize,
                 fontWeight: FontWeight.bold,
               ),
             ),
