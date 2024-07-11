a961b532cb5b3679dc2b35185b9351c75ca208e8
thwlstn <thwlstn23@gmail.com>
2024-07-11 23:31:54 +0900

    feat : #76 로그인 API 연동 & 토큰 재발급 API 연동
    [ 로그인 API ]
    - 로그인 시 토큰이 Null이라는 오류가 있었는데 접근 방식을 response.body가 아니라 response.headers로 접근해 accessToken과 refreshToken 추출 다른 API에서도 방식은 동일
    - 자동 로그인 체크 여부에 따라 앱 종료 후 다시 접속하면 로그인 상태가 유지되거나 초기화 되도록 구현
    - refreshToken은 쿠키 안에 저장한 후 관리되도록 설정
    
    [ 토큰 재발급 API ]
    - accessToken이 만료되면 토큰 재발급 API를 호출해 쿠키에 저장해 둔 refreshToken을 통해 새로운 토큰을 발급 받을 수 있도록 구현
    - accessToken이 만료되지 않은 경우에 만료될 때까지 로그인 유지
    - 토큰 재발급 받을 경우 그 전에 가지고 있던 토큰은 삭제되도록 구현

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -1,8 +1,10 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:frontend/screens/settingProFile1_screen.dart';
 import 'package:http/http.dart' as http;
+import 'package:cookie_jar/cookie_jar.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -17,15 +19,144 @@ class _LoginPageState extends State<LoginPage> {
 
   bool isAutoLogin = false;
   bool pwInvisible = true; // 비밀번호 숨김 기본값 true
+  late PersistCookieJar cookieJar; // 쿠키를 관리할 객체
 
   final formKey = GlobalKey<FormState>(); // 폼 유효성을 검사하는데 사용
 
   // 로그인 및 토큰 재발급을 위한 서버 주소 상수
   static const loginAddress = 'https://reminder.sungkyul.ac.kr/login';
+  static const tokenRefreshAddress =
+      'https://reminder.sungkyul.ac.kr/api/v1/reissue';
 
   @override
   void initState() {
     super.initState();
+    _initCookieJar(); // 초기화 함수 호출
+    _autoLogin(); // 자동 로그인 시도
+  }
+
+  // refreshToken 추출 메서드
+  String? extractRefreshToken(String setCookieHeader) {
+    final regex = RegExp(r'refresh=([^;]+)');
+    final match = regex.firstMatch(setCookieHeader);
+    return match?.group(1);
+  }
+
+  // 쿠키 관리를 위한 초기화 함수
+  Future<void> _initCookieJar() async {
+    final appDocDir =
+        await getApplicationDocumentsDirectory(); // 앱의 문서 디렉토리 경로 가져오기
+    final appDocPath = appDocDir.path; // 디렉토리 경로 저장
+    print('쿠키 저장 경로: $appDocPath/.cookies/'); // 디버그 출력
+    cookieJar = PersistCookieJar(
+      storage: FileStorage('$appDocPath/.cookies/'), // 파일 저장소로 쿠키 저장
+    );
+  }
+
+  // 저장된 학번과 비밀번호 불러오기
+  Future<void> _loadCredentials() async {
+    final prefs = await SharedPreferences.getInstance();
+    final studentId = prefs.getString('studentId');
+    final password = prefs.getString('password');
+    final autoLogin = prefs.getBool('isAutoLogin') ?? false;
+
+    if (studentId != null && password != null && autoLogin) {
+      setState(() {
+        idController.text = studentId;
+        pwController.text = password;
+        isAutoLogin = autoLogin;
+      });
+    }
+  }
+
+  // 자동 로그인 시도 함수
+  Future<void> _autoLogin() async {
+    await _loadCredentials();
+    final prefs = await SharedPreferences.getInstance();
+    final accessToken = prefs.getString('accessToken');
+    final studentId = prefs.getString('studentId');
+    final password = prefs.getString('password');
+
+    if (accessToken != null && studentId != null && password != null) {
+      final isExpired = JwtDecoder.isExpired(accessToken);
+      if (isExpired) {
+        await againToken(); // 토큰 재발급 시도
+      } else {
+        print('유효한 토큰이 존재합니다.');
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const SettingProfile1Page(),
+          ),
+        );
+      }
+    }
+  }
+
+  // 이전 토큰 삭제 함수
+  Future<void> clearTokens() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('accessToken');
+    await prefs.remove('refreshToken');
+    await cookieJar.deleteAll(); // 쿠키 삭제
+  }
+
+  // 토큰 재발급 API
+  Future<void> againToken() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final refreshToken = prefs.getString('refreshToken') ?? '';
+
+      if (refreshToken.isEmpty) {
+        print('invalid refresh token');
+        return;
+      }
+
+      final url = Uri.parse(tokenRefreshAddress);
+      final cookieHeader = 'refresh=$refreshToken';
+
+      final response = await http.post(url, headers: {
+        'Cookie': cookieHeader,
+      });
+
+      print('토큰 재발급 응답 상태 코드: ${response.statusCode}');
+
+      if (response.statusCode == 200) {
+        final newAccessToken = response.headers['access'];
+        final setCookieHeader = response.headers['set-cookie'];
+        print('새로운 엑세스 토큰 $newAccessToken');
+
+        final newRefreshToken = setCookieHeader != null
+            ? extractRefreshToken(setCookieHeader)
+            : null;
+        print('새로운 리프래시 토큰 $newRefreshToken');
+
+        if (newAccessToken != null && newRefreshToken != null) {
+          await clearTokens();
+
+          await prefs.setString('accessToken', newAccessToken);
+          await prefs.setString('refreshToken', newRefreshToken);
+
+          final uri = Uri.parse(tokenRefreshAddress);
+          cookieJar.saveFromResponse(uri, [Cookie('refresh', newRefreshToken)]);
+        } else {
+          print('응답 데이터에 토큰이 없습니다');
+        }
+
+        print('토큰 재발급 성공!');
+        // 토큰 재발급 성공 시
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const SettingProfile1Page(),
+          ),
+        );
+      } else {
+        print('토큰 재발급 실패: ${response.statusCode} ${response.body}');
+      }
+    } catch (e) {
+      print('토큰 재발급 요청 중 에러 발생: ${e.toString()}');
+    }
   }
 
   // 로그인 API
@@ -33,6 +164,7 @@ class _LoginPageState extends State<LoginPage> {
     try {
       final url = Uri.parse(loginAddress);
 
+      // HTTP POST 요청 전송
       final response = await http.post(
         url,
         body: {
@@ -45,22 +177,50 @@ class _LoginPageState extends State<LoginPage> {
       print('로그인 응답 본문: ${response.body}');
 
       if (response.statusCode == 200) {
-        final responseData = jsonDecode(response.body);
-        final accessToken = responseData['accessToken'];
-        final refreshToken = responseData['refreshToken'];
+        final accessToken = response.headers['access']; // 액세스 토큰 추출
+        final setCookieHeader = response.headers['set-cookie'];
+        final refreshToken = setCookieHeader != null
+            ? extractRefreshToken(setCookieHeader)
+            : null;
 
         final prefs = await SharedPreferences.getInstance();
         if (accessToken != null && refreshToken != null) {
-          await prefs.setString('accessToken', accessToken);
-        }
+          // 이전 토큰 삭제
+          await clearTokens();
 
+          // 새 토큰 저장
+          await prefs.setString('accessToken', accessToken); // 액세스 토큰 저장
+          await prefs.setString('refreshToken', refreshToken); // 리프레시 토큰 저장
+
+          // 자동 로그인 상태 저장
+          await prefs.setBool('isAutoLogin', isAutoLogin);
+          if (isAutoLogin) {
+            // 자동 로그인 체크 시에만 학번과 비밀번호 저장
+            await prefs.setString('studentId', studentId);
+            await prefs.setString('password', password);
+          } else {
+            // 체크 해제 시 저장된 학번과 비밀번호 삭제
+            await prefs.remove('studentId');
+            await prefs.remove('password');
+          }
+
+          final uri = Uri.parse(loginAddress);
+          cookieJar.saveFromResponse(uri,
+              [Cookie('refreshToken', refreshToken)]); // refreshToken 쿠키 저장
+
+          // 저장된 토큰 로그로 확인
+          final savedAccessToken = prefs.getString('accessToken');
+          final savedRefreshToken = prefs.getString('refreshToken');
+          print('저장된 액세스 토큰: $savedAccessToken');
+          print('저장된 리프레시 토큰: $savedRefreshToken');
+        }
         print('로그인 성공');
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(
             builder: (context) => const SettingProfile1Page(),
           ),
-        );
+        ); // 로그인 성공 후 홈 화면으로 이동
       } else {
         print('로그인 실패: ${response.statusCode} ${response.body}');
       }
@@ -199,10 +359,16 @@ class _LoginPageState extends State<LoginPage> {
                     children: [
                       Checkbox(
                         value: isAutoLogin,
-                        onChanged: (value) {
+                        onChanged: (value) async {
                           setState(() {
-                            isAutoLogin = !isAutoLogin;
+                            isAutoLogin = value!;
                           });
+                          final prefs = await SharedPreferences.getInstance();
+                          await prefs.setBool('isAutoLogin', isAutoLogin);
+                          if (!isAutoLogin) {
+                            await prefs.remove('studentId');
+                            await prefs.remove('password');
+                          }
                         },
                         activeColor: const Color(0xFF2A72E7),
                         materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
