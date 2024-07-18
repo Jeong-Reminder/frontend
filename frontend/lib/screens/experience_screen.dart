8fe985e15450dcc0d221e4a692482615e4715b46
thwlstn <thwlstn23@gmail.com>
2024-07-18 13:08:31 +0900

    feat : #99 내 프로젝트 경험 조회 API 연동 [경험 수정하기 버튼 추가 구현] - 사용자 경험 정보를 ExpansionTile을 사용하여 리스트로 표시되도록 구현 - 각 경험 항목의 초기 확장 상태를 false로 설정 - ExpansionTile 클릭 시 확장 상태 저장 및 반영 - 경험 수정 버튼의 가시성 상태 추가

--- frontend/lib/screens/experience_screen.dart
+++ frontend/lib/screens/experience_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/models/projectExperience_model.dart';
 
-class ExperiencePage extends StatelessWidget {
+class ExperiencePage extends StatefulWidget {
   final List<ProjectExperience> experiences;
 
   const ExperiencePage({required this.experiences, super.key});
+
+  @override
+  ExperiencePageState createState() => ExperiencePageState();
+}
+
+class ExperiencePageState extends State<ExperiencePage> {
+  // 각 ExpansionTile의 확장 상태를 저장하는 리스트
+  late List<bool> _isExpanded;
+
+  @override
+  void initState() {
+    super.initState();
+    // 각 경험 항목의 초기 확장 상태를 false로 설정
+    _isExpanded = List<bool>.filled(widget.experiences.length, false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,58 +57,100 @@ class ExperiencePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              '✨소진수의 빛나는 경험✨',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+            Row(
+              children: [
+                const Text(
+                  '✨소진수의 빛나는 경험✨',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                const Spacer(),
+                Visibility(
+                  visible: _isExpanded.contains(true),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 20,
+                      width: 100,
+                      margin: const EdgeInsets.only(left: 10),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF2A72E7),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '경험 수정하기',
+                          style: TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
             const SizedBox(height: 20),
             Expanded(
               child: ListView.builder(
-                itemCount: experiences.length,
+                itemCount: widget.experiences.length,
                 itemBuilder: (context, index) {
-                  final experience = experiences[index];
-                  return ExpansionTile(
-                    title: userInfo(
-                      title: '프로젝트명',
-                      titleSize: 20,
-                      info: experience.experienceName,
-                    ),
+                  final experience = widget.experiences[index];
+                  return Column(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                        child: Column(
-                          children: [
-                            userInfo(
-                              title: '나의 역할',
-                              titleSize: 18,
-                              info: experience.experienceRole,
-                            ),
-                            userInfo(
-                              title: '프로젝트 경험',
-                              titleSize: 18,
-                              info: experience.experienceContent,
-                            ),
-                            userInfo(
-                              title: '깃허브 프로젝트 링크',
-                              titleSize: 18,
-                              info: experience.experienceGithub,
-                            ),
-                            userInfo(
-                              title: '직무',
-                              titleSize: 18,
-                              info: experience.experienceJob,
-                            ),
-                            userInfo(
-                              title: '프로젝트 기간',
-                              titleSize: 18,
-                              info: experience.experienceDate,
-                            ),
-                          ],
+                      ExpansionTile(
+                        title: userInfo(
+                          title: '프로젝트명',
+                          titleSize: 20,
+                          info: experience.experienceName,
                         ),
+                        onExpansionChanged: (bool expanded) {
+                          setState(() {
+                            _isExpanded[index] = expanded;
+                          });
+                        },
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 20.0),
+                            child: Column(
+                              children: [
+                                userInfo(
+                                  title: '나의 역할',
+                                  titleSize: 18,
+                                  info: experience.experienceRole,
+                                ),
+                                userInfo(
+                                  title: '프로젝트 경험',
+                                  titleSize: 18,
+                                  info: experience.experienceContent,
+                                ),
+                                userInfo(
+                                  title: '깃허브 프로젝트 링크',
+                                  titleSize: 18,
+                                  info: experience.experienceGithub,
+                                ),
+                                userInfo(
+                                  title: '직무',
+                                  titleSize: 18,
+                                  info: experience.experienceJob,
+                                ),
+                                userInfo(
+                                  title: '프로젝트 기간',
+                                  titleSize: 18,
+                                  info: experience.experienceDate,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
+                      const SizedBox(height: 10),
                     ],
                   );
                 },
@@ -106,10 +163,11 @@ class ExperiencePage extends StatelessWidget {
   }
 
   // 회원 정보 위젯
-  Widget userInfo(
-      {required String title,
-      required double titleSize,
-      required String info}) {
+  Widget userInfo({
+    required String title,
+    required double titleSize,
+    required String info,
+  }) {
     return Column(
       children: [
         ListTile(
