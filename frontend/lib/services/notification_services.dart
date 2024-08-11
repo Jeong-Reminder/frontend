d4270536f59b5feef5810cdc0cae92e77646f12a
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-08-11 22:38:26 +0900

    feat : #124 하나의 게시글 조회 API 연동
    - 게시글 아이디를 받아 api 연동
    - board_widget에서 ontap으로 api 호출 후 detailBoardPage로 이동

--- frontend/lib/services/notification_services.dart
+++ frontend/lib/services/notification_services.dart
@@ -13,7 +13,9 @@ class NotificationService {
     // const String baseUrl =
     //     'https://reminder.sungkyul.ac.kr/api/v1/notifications/test-send';
     const String baseUrl =
-        'http://127.0.0.1:9000/api/v1/notifications/test-send';
+        'http://10.0.2.2:9000/api/v1/notifications/test-send';
+    // const String baseUrl =
+    // 'http://127.0.0.1:9000/api/v1/notifications/test-send';
 
     print('fcmToken: $fcmToken');
 
