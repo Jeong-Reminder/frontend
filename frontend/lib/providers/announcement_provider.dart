68fc77d370c444ce45d18d7f5a0e6d3d675ef0e8
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-08-27 04:33:43 +0900

    feat : #133 IOS 파일 다운로드 API 연동
    - share_plus 패키지를 사용해서 다른 앱으로 공유 가능하게 작성(android도 똑같이 적용)

--- frontend/lib/providers/announcement_provider.dart
+++ frontend/lib/providers/announcement_provider.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:frontend/models/board_model.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
@@ -376,6 +377,7 @@ class AnnouncementProvider with ChangeNotifier {
     }
   }
 
+  // 파일 다운로드
   Future<void> downloadFile(String url, String fileName) async {
     try {
       // 안드로이드 에뮬레이터에서 localhost를 10.0.2.2로 대체
@@ -389,22 +391,36 @@ class AnnouncementProvider with ChangeNotifier {
       final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
-        // 권한 요청
-        if (await Permission.manageExternalStorage.request().isGranted) {
-          // Downloads 디렉토리에 파일 저장
-          Directory downloadsDir = Directory('/storage/emulated/0/Download');
-          String downloadsPath = path.join(downloadsDir.path, fileName);
+        Directory? downloadsDir;
 
+        if (Platform.isAndroid) {
+          // Android에서의 파일 저장 경로
+          if (await Permission.manageExternalStorage.request().isGranted) {
+            downloadsDir = Directory('/storage/emulated/0/Download');
+          } else {
+            print('스토리지 접근 권한이 필요합니다.');
+            bool shouldOpenSettings = await Permission.storage.isDenied;
+            if (shouldOpenSettings) {
+              openAppSettings();
+            }
+            return;
+          }
+        } else if (Platform.isIOS) {
+          // iOS에서의 파일 저장 경로
+          downloadsDir = await getApplicationDocumentsDirectory();
+        }
+
+        if (downloadsDir != null) {
+          String downloadsPath = path.join(downloadsDir.path, fileName);
           final file = File(downloadsPath);
           await file.writeAsBytes(response.bodyBytes);
-
           print('파일이 다운로드되었습니다: $downloadsPath');
+
+          // 파일을 다른 앱으로 공유 (Files 앱 포함)
+          final xFile = XFile(downloadsPath);
+          Share.shareXFiles([xFile], text: 'Downloaded file: $fileName');
         } else {
-          print('스토리지 접근 권한이 필요합니다.');
-          bool shouldOpenSettings = await Permission.storage.isDenied;
-          if (shouldOpenSettings) {
-            openAppSettings();
-          }
+          print('파일 경로를 찾을 수 없습니다.');
         }
       } else {
         print('파일 다운로드 실패: 상태 코드 ${response.statusCode}');
