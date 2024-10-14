e8abb88570e15869e04c129e14f532bf645deb9d
Taek Ki Min <127868094+TaekkiMin@users.noreply.github.com>
2024-10-14 17:35:43 +0900

    fix : #214 푸시 알림 탭할 때 상세 페이지 혹은 모집 상세 페이지 이동 구현

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,77 +3,72 @@ import 'package:flutter/services.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:frontend/admin/providers/admin_provider.dart';
 import 'package:frontend/providers/announcement_provider.dart';
+import 'package:frontend/providers/makeTeam_provider.dart';
+import 'package:frontend/providers/profile_provider.dart';
+import 'package:frontend/providers/projectExperience_provider.dart';
 import 'package:frontend/providers/teamApply_provider.dart';
 import 'package:frontend/providers/vote_provider.dart';
 import 'package:frontend/screens/boardDetail_screen.dart';
-import 'package:frontend/screens/corSeaBoard_screen.dart';
-import 'package:frontend/screens/gradeBoard_screen.dart';
-import 'package:frontend/screens/contestBoard_screen.dart';
-import 'package:frontend/screens/editField_screen.dart';
-import 'package:frontend/screens/editTool_screen.dart';
-import 'package:frontend/screens/hiddenList_screen.dart';
-import 'package:frontend/screens/myOwnerPage_screen.dart';
-import 'package:frontend/screens/totalBoard_screen.dart';
-import 'package:get/get.dart';
-import 'package:frontend/admin/providers/admin_provider.dart';
-import 'package:frontend/admin/screens/addMember_screen.dart';
-import 'package:frontend/admin/screens/dashboard_screen.dart';
-import 'package:frontend/admin/screens/userInfo_screen.dart';
-import 'package:frontend/providers/profile_provider.dart';
-import 'package:frontend/providers/projectExperience_provider.dart';
-import 'package:frontend/providers/makeTeam_provider.dart';
-import 'package:frontend/screens/experience_screen.dart';
-import 'package:frontend/screens/home_screen.dart';
 import 'package:frontend/screens/login_screen.dart';
-import 'package:frontend/screens/myUserPage_screen.dart';
-import 'package:frontend/screens/setProfile_screen.dart';
-import 'package:frontend/screens/setExperience_screen.dart';
+import 'package:frontend/screens/recruitDetail_screen.dart';
+import 'package:get/get.dart';
 import 'package:frontend/services/login_services.dart';
 import 'package:provider/provider.dart';
 
-// 알림 플러그인 인스턴스 생성
+// 알림 플러그인 인스턴스 생성 (로컬 푸시 알림을 보여주기 위한 플러그인)
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
-// 백그라운드에서 수신된 FCM 메시지 핸들러
+// 백그라운드에서 수신된 FCM 메시지 핸들러 (앱이 백그라운드에서 알림을 받을 때 처리)
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(); // Firebase 재초기화
   print('Handling a background message: ${message.messageId}');
-  _showNotification(message); // 수정된 부분: 이제 전역 함수로 접근 가능
+  _showNotification(message); // 백그라운드 메시지를 수신하면 알림 표시
 }
 
-// 전역 함수로 이동
+// 알림을 보여주는 함수 (전역 함수로 설정하여 백그라운드에서도 사용 가능)
 void _showNotification(RemoteMessage message) async {
+  // Android용 알림 설정
   var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
-    'high_importance_channel',
-    'High Importance Notifications',
+    'high_importance_channel', // 알림 채널 ID
+    'High Importance Notifications', // 알림 채널 이름
     channelDescription: 'This channel is used for important notifications.',
-    icon: '@mipmap/ic_launcher',
+    icon: '@mipmap/ic_launcher', // 알림 아이콘 설정
   );
+
+  // iOS용 알림 설정
   var iOSPlatformChannelSpecifics = const DarwinNotificationDetails(
-    badgeNumber: 1,
-    subtitle: 'the subtitle',
-    sound: 'slow_spring_board.aiff',
+    badgeNumber: 1, // 배지 번호 설정
+    subtitle: 'the subtitle', // 서브 타이틀 설정
+    sound: 'slow_spring_board.aiff', // 알림 사운드 설정
   );
+
+  // 플랫폼별 알림 설정 통합
   var platformChannelSpecifics = NotificationDetails(
     android: androidPlatformChannelSpecifics,
     iOS: iOSPlatformChannelSpecifics,
   );
 
+  // payload에 targetId와 category 추가
+  String payload = '${message.data['targetId']},${message.data['category']}';
+
+  // 알림 표시
   await flutterLocalNotificationsPlugin.show(
-    message.hashCode,
-    message.notification?.title,
-    message.notification?.body,
-    platformChannelSpecifics,
+    message.hashCode, // 알림 ID
+    message.notification?.title, // 알림 제목
+    message.notification?.body, // 알림 내용
+    platformChannelSpecifics, // 알림 설정
+    payload: payload, // 알림 탭시 사용할 데이터
   );
 }
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized(); // Flutter의 비동기적 초기화
 
   try {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(); // Firebase 초기화 시도
     print("Firebase initialized successfully");
   } catch (e) {
     print("Firebase initialization error: $e");
@@ -89,17 +84,15 @@ void main() async {
   // 백그라운드 메시지 핸들러 등록
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-  // 알림 초기화 설정
+  // 알림 초기화 설정(Android 및 iOS용)
   const AndroidInitializationSettings initializationSettingsAndroid =
       AndroidInitializationSettings('@mipmap/ic_launcher');
   DarwinInitializationSettings initializationSettingsDarwin =
       DarwinInitializationSettings(
-    onDidReceiveLocalNotification: (id, title, body, payload) async {
-      // iOS에서 로컬 알림을 받을 때 처리할 작업
-      print('Notification received: $title $body');
-      // 여기에 필요한 동작을 추가하세요.
-    },
+    onDidReceiveLocalNotification: (id, title, body, payload) async {},
   );
+
+  // 알림 초기화 설정 통합(Android 및 iOS)
   InitializationSettings initializationSettings = InitializationSettings(
     android: initializationSettingsAndroid,
     iOS: initializationSettingsDarwin,
@@ -109,10 +102,27 @@ void main() async {
   flutterLocalNotificationsPlugin.initialize(initializationSettings,
       onDidReceiveNotificationResponse: (response) {
     if (response.payload != null) {
-      Get.to(const HomePage(), arguments: response.payload); // 임시로 홈페이지 이동
+      _onNotificationTap(response.payload!); // 알림 탭시 이동 처리
     }
   });
 
+  // 백그라운드, 포그라운드 상태에서 알림 클릭 처리 설정
+  await setupInteractedMessage();
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Notification id: ${message.data['targetId']}');
+      print('Notification category: ${message.data['category']}');
+
+      // 알림을 즉시 이동하지 않고, 표시만 함
+      _showNotification(message);
+    }
+  });
+
+  // 앱 실행 시 Provider로 여러 상태 관리 클래스 주입
   runApp(
     MultiProvider(
       providers: [
@@ -127,9 +137,6 @@ void main() async {
       child: const MyApp(),
     ),
   );
-
-  // 백그라운드 메시지 클릭 액션 설정
-  await setupInteractedMessage();
 }
 
 class MyApp extends StatefulWidget {
@@ -141,26 +148,14 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   final LoginAPI loginAPI = LoginAPI(); // LoginAPI 인스턴스 생성
+  RemoteMessage? initializeMessage; // 초기 메세지 변수 선언
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this); // 앱 라이프사이클 이벤트 관찰 시작
 
-    // 알림 채널 설정
     _createNotificationChannel();
-
-    // 포그라운드 메시지 처리
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
-
-      if (message.notification != null) {
-        print(
-            'Message also contained a notification: ${message.notification!.title}, ${message.notification!.body}');
-        _showNotification(message);
-      }
-    });
   }
 
   @override
@@ -169,24 +164,26 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
     super.dispose();
   }
 
+  // 앱의 라이프사이클 상태 변화 감지 (예: 앱 종료 시)
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) async {
     print("AppLifecycleState changed: $state");
     if (state == AppLifecycleState.detached) {
-      print("App is detaching, calling logout");
+      print("App is detaching");
       // _loginAPI.logoutOnExit(); // 앱이 종료될 때 로그아웃 호출
     }
   }
 
+  // 중요한 알림을 받을 채널 생성(Android)
   void _createNotificationChannel() async {
     var channel = const AndroidNotificationChannel(
-      'high_importance_channel', // id
-      'High Importance Notifications', // name
-      description:
-          'This channel is used for important notifications.', // description
-      importance: Importance.high,
+      'high_importance_channel', // 채널 ID
+      'High Importance Notifications', // 채널 이름
+      description: 'This channel is used for important notifications.', // 채널 설명
+      importance: Importance.high, // 높은 우선순위
     );
 
+    // Android 알림 채널 생성
     await flutterLocalNotificationsPlugin
         .resolvePlatformSpecificImplementation<
             AndroidFlutterLocalNotificationsPlugin>()
@@ -195,73 +192,125 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
+    // 세로 모드 고정
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
-    ]); // 가로모드 방지(세로모드 지원)
+    ]);
+
+    // 화면 크기 계산
     _screenSize();
 
-    return MaterialApp(
+    return GetMaterialApp(
       initialRoute: '/',
-      routes: {
-        '/': (context) => const LoginPage(),
-        '/add_member': (context) => const AddMemberPage(),
-        '/user-info': (context) => const UserInfoPage(),
-        '/myuser': (context) => const MyUserPage(),
-        '/myowner': (context) => const MyOwnerPage(),
-        '/set-profile': (context) => const SetProfilePage(),
-        '/member-experience': (context) => const SetExperiencePage(),
-        '/experience': (context) => const ExperiencePage(
-              experiences: [],
-              name: '',
-            ),
-        '/homepage': (context) => const HomePage(),
-        '/dashboard': (context) => const DashBoardPage(),
-        '/edit-field': (context) => const EditFieldPage(),
-        '/edit-tool': (context) => const EditToolPage(),
-        '/total-board': (context) => const TotalBoardPage(),
-        '/contest-board': (context) => const ContestBoardPage(),
-        '/grade-board': (context) => const GradeBoardPage(),
-        '/corSea-board': (context) => const CorSeaBoardPage(),
-        '/detail-board': (context) => const BoardDetailPage(),
-        '/hidden-board': (context) => HiddenPage(),
-      },
+      getPages: [
+        GetPage(name: '/', page: () => const LoginPage()), // 로그인 페이지
+        GetPage(
+            name: '/detail-board',
+            page: () => const BoardDetailPage()), // 추가로 명시
+        GetPage(
+            name: '/detail-recruit',
+            page: () => const RecruitDetailPage()), // 추가로 명시
+      ],
     );
   }
 
+  // 화면 크기를 계산하여 장치에 따른 반응형 레이아웃 구현 가능
   _screenSize() {
     MediaQueryData mediaQueryData = MediaQueryData.fromView(View.of(context));
-    // 화면의 가로 및 세로 크기를 가져옵니다.
     double screenWidth = mediaQueryData.size.width;
     double screenHeight = mediaQueryData.size.height;
-    if (screenWidth > 600) {
-      // print('pad 에서 접속했습니다. 가로세로모두 적용');
-    } else {
-      // print('phone 에서 접속했습니다. 세로모드만 적용');
-    }
-    // print('>>screenWidth');
-    print(screenWidth);
-    print(screenHeight);
+
+    print('screenWidth: $screenWidth');
+    print('screenHeight : $screenHeight');
   }
 }
 
-// 백그라운드 메시지 클릭 액션 설정
+// 포그라운드, 백그라운드 메시지 클릭 시 실행되는 핸들러 설정
 Future<void> setupInteractedMessage() async {
+  // 앱이 종료된 상태에서 알림을 클릭하여 시작된 경우의 메시지 처리
   RemoteMessage? initialMessage =
       await FirebaseMessaging.instance.getInitialMessage();
 
-  // 종료상태에서 클릭한 푸시 알림 메시지 핸들링
+  print('initialMessage: $initialMessage');
+
   if (initialMessage != null) {
-    _handleMessage(initialMessage);
+    print('Initial message received: ${initialMessage.data}');
+
+    // 알림 데이터에서 필요한 정보를 추출하여 페이지로 이동
+    _exitNavigateBoard(initialMessage.data);
   }
 
-  // 앱이 백그라운드 상태에서 푸시 알림 클릭 하여 열릴 경우 메시지 스트림을 통해 처리
-  FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  _navigateBoard();
 }
 
-void _handleMessage(RemoteMessage message) {
-  print('message = ${message.notification!.title}');
-  if (message.data['type'] == 'chat') {
-    Get.toNamed('/detail-board', arguments: message.data);
+void _exitNavigateBoard(Map<String, dynamic> messageData) {
+  final int id = int.parse(messageData['targetId'].toString());
+  final String category = messageData['category'].toString();
+
+  print("Navigating to BoardDetailPage with ID: $id, Category: $category");
+
+  Get.toNamed(
+    '/detail-board',
+    arguments: {'announcementId': id, 'category': category},
+    preventDuplicates: false,
+  );
+}
+
+// id와 category를 받아서 상세 페이지로 이동하는 메서드
+void _navigateBoard() async {
+  // MethodChannel을 통한 네이티브 코드에서 데이터를 수신
+  const platform = MethodChannel("com.sungkyul/notification");
+
+  platform.setMethodCallHandler((MethodCall call) async {
+    if (call.method == 'navigateToDetail') {
+      print(
+          "Method channel called with arguments: ${call.arguments}"); // call.arguments는 AppDelegate의 notificationData를 뜻함
+
+      final Map<String, dynamic> args =
+          Map<String, dynamic>.from(call.arguments);
+      final int id = int.parse(args['id'].toString());
+      final String category = args['category'].toString();
+
+      print("Received ID: $id, Category: $category");
+
+      if (category == '공지') {
+        Get.toNamed(
+          '/detail-board',
+          arguments: {'announcementId': id, 'category': category},
+          preventDuplicates: false,
+        );
+      } else if (category == '팀원모집') {
+        Get.toNamed(
+          '/detail-recruit',
+          arguments: {'makeTeamId': id},
+          preventDuplicates: false,
+        );
+      }
+    }
+  });
+}
+
+void _onNotificationTap(String payload) {
+  // payload에서 targetId와 category 추출
+  List<String> data = payload.split(',');
+  int targetId = int.parse(data[0]);
+  String category = data[1];
+
+  print(
+      "Navigating to BoardDetailPage with ID: $targetId, Category: $category");
+
+  if (category == '공지') {
+    Get.toNamed(
+      '/detail-board',
+      arguments: {'announcementId': targetId, 'category': category},
+      preventDuplicates: false,
+    );
+  } else if (category == '팀원모집') {
+    Get.toNamed(
+      '/detail-recruit',
+      arguments: {'makeTeamId': targetId},
+      preventDuplicates: false,
+    );
   }
 }
