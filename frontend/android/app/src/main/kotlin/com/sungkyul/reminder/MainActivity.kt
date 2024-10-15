38e2f038a4b7523143a163d5da867dbdb446802e
thwlstn <thwlstn23@gmail.com>
2024-10-16 00:56:05 +0900

    fix : develop MainActivity.kt 파일 오류 수정

--- frontend/android/app/src/main/kotlin/com/sungkyul/reminder/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/sungkyul/reminder/MainActivity.kt
@@ -3,7 +3,10 @@ package com.sungkyul.reminder
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "com.sungkyul/notification"
@@ -15,18 +18,23 @@ class MainActivity: FlutterActivity() {
             if (call.method == "navigateToDetail") {
                 val id = call.argument<Int>("id")
                 val category = call.argument<String>("category")
-                
-                // 이곳에서 Flutter로 전달할 데이터를 추가
-                result.success(mapOf("id" to id, "category" to category))
+
+                if (id != null && category != null) {
+                    // 성공적으로 데이터를 Flutter로 전달
+                    result.success(mapOf("id" to id, "category" to category))
+                } else {
+                    // 데이터가 null이면 오류 처리
+                    result.error("INVALID_ARGUMENTS", "Arguments are missing", null)
+                }
+            } else {
+                result.notImplemented()
             }
         }
     }
-    
-    override fun onCreate(savedInstanceState: android.os.Bundle?) {
-        super.onCreate(savedInstanceState)
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         // 알림 채널 설정
-        // 안드로이드 버전이 오레오(API 26) 이상인 경우에만 실행
         createNotificationChannel()
     }
 
@@ -38,7 +46,7 @@ class MainActivity: FlutterActivity() {
             val channel = NotificationChannel("high_importance_channel", name, importance)
             channel.description = descriptionText
 
-            // 알림 매니저(알림 채널의 이름, 설명 및 중요도를 설정)를 통해 채널 등록
+            // 알림 매니저를 통해 채널 등록
             val notificationManager: NotificationManager = getSystemService(NotificationManager::class.java)
             notificationManager.createNotificationChannel(channel)
         }
